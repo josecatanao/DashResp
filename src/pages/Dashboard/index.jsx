57f0684900477f775a78a09aff5117ff16d12bf5
josecatanao <51245690+josecatanao@users.noreply.github.com>
2022-05-09 12:07:53 -0300

    Adicionando filtros dos UFs no front

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -2,9 +2,39 @@ import "./style.css";
 import Menu from "../../components/menu"
 function Dashboard() {
   return (
-        <div>
-            <Menu />
-        </div>
+    <div>
+      <Menu />
+      <h4 className="titulo-filtro">Filtrar por UF:</h4>
+      <div id="menu-dash">
+       <button>AC</button> 
+       <button>AL</button>
+       <button>AP</button>
+       <button>AM</button>
+       <button>BA</button>
+       <button>CE</button>
+       <button>DF</button>
+       <button>ES</button>
+       <button>GO</button>
+       <button>MA</button>
+       <button>MT</button>
+       <button>MS</button>
+       <button>MG</button>
+       <button>PA</button>
+       <button>PB</button>
+       <button>PR</button>
+       <button>PE</button>
+       <button>PI</button>
+       <button>RJ</button>
+       <button>RN</button>
+       <button>RS</button>
+       <button>RO</button>
+       <button>RR</button>
+       <button>SC</button>
+       <button>SP</button>
+       <button>SE</button>
+       <button>TO</button>
+      </div>
+    </div>
   )
 }
 
