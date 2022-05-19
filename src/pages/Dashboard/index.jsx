efbb0a1a2d39f11a6c2d5673aa6e495799960a64
josecatanao <51245690+josecatanao@users.noreply.github.com>
2022-05-19 12:09:11 -0300

    Melhorando os filtros e gráficos

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,94 +1,83 @@
 import "./style.css";
 import Menu from "../../components/menu"
 import ChartsEmbedSDK from "@mongodb-js/charts-embed-dom";
+import { useForm } from 'react-hook-form';
 import { useEffect } from "react";
 
-const sdk = new ChartsEmbedSDK();
-const chart1 = sdk.createChart({
-  baseUrl: "https://charts.mongodb.com/charts-project-0-ylbgo",
-  chartId: "627a746b-d1c5-4167-8c17-ecdd4e5710fd",
-  width: "700px",
-  height: "300px"
-});
- 
-
-const chart2 = sdk.createChart({
-  baseUrl: "https://charts.mongodb.com/charts-project-0-ylbgo",
-  chartId: "627a6814-64ce-4b97-8476-5bcedf270e95",
-  width: "350px",
-  height: "150px"
-}); 
-
-const chart3 = sdk.createChart({
-  baseUrl: "https://charts.mongodb.com/charts-project-0-ylbgo",
-  chartId: "627a81d9-d1c5-41ad-8b57-ecdd4e6e4f55",
-  width: "350px",
-  height: "150px"
-});
-
-const filtrar = (valor) => {
-  var lista = document.getElementsByClassName("fitro-selecionado");
-  for (var i = lista.length - 1; i >= 0; i--) {
-    lista[i].classList.remove("fitro-selecionado") 
-  }
-  var button = document.getElementById(valor);
-  button.classList.add("fitro-selecionado");
-  chart1.setFilter({ "UF": valor });
-  chart2.setFilter({ "UF": valor });
-  chart3.setFilter({ "UF": valor });
-}
-const limpar = () => {
-  var lista = document.getElementsByClassName("fitro-selecionado");
-  for (var i = lista.length - 1; i >= 0; i--) {
-    lista[i].classList.remove("fitro-selecionado") 
-  }
-  chart1.setFilter({});
-  chart2.setFilter({});
-  chart3.setFilter({});
-}
-
+const sdk = new ChartsEmbedSDK({ baseUrl: "https://charts.mongodb.com/charts-project-0-ylbgo" });
+const chart1 = sdk.createChart({ chartId: "627a746b-d1c5-4167-8c17-ecdd4e5710fd" });
+const chart2 = sdk.createChart({ chartId: "627a6814-64ce-4b97-8476-5bcedf270e95" });
+const chart3 = sdk.createChart({ chartId: "627a81d9-d1c5-41ad-8b57-ecdd4e6e4f55" });
 
 
 function Dashboard() {
-  useEffect(()=>{
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (e) => {
+    console.log(e)
+    chart1.setFilter(e);
+    chart2.setFilter(e);
+    chart3.setFilter(e);
+  }
+
+
+  useEffect(() => {
+    //Valor comprado por UF
     chart1.render(document.getElementById("chart1"))
+    //Valor total das compras dos respiradores
     chart2.render(document.getElementById("chart2"))
+    //QTD. total de respiradores comprados
     chart3.render(document.getElementById("chart3"))
   })
-  
+
   return (
     <div>
       <Menu />
-      <h4 className="titulo-filtro">Filtrar por UF:</h4>
+      <h4 className="titulo-filtro">Filtros:</h4>
       <div id="menu-dash">
-        <button id="AC" onClick={() => { filtrar("AC") }}>AC</button>
-        <button id="AL" onClick={() => { filtrar("AL") }}>AL</button>
-        <button id="AP" onClick={() => { filtrar("AP") }}>AP</button>
-        <button id="AM" onClick={() => { filtrar("AM") }}>AM</button>
-        <button id="BA" onClick={() => { filtrar("BA") }}>BA</button>
-        <button id="CE" onClick={() => { filtrar("CE") }}>CE</button>
-        <button id="DF" onClick={() => { filtrar("DF") }}>DF</button>
-        <button id="ES" onClick={() => { filtrar("ES") }}>ES</button>
-        <button id="GO" onClick={() => { filtrar("GO") }}>GO</button>
-        <button id="MA" onClick={() => { filtrar("MA") }}>MA</button>
-        <button id="MT" onClick={() => { filtrar("MT") }}>MT</button>
-        <button id="MS" onClick={() => { filtrar("MS") }}>MS</button>
-        <button id="MG" onClick={() => { filtrar("MG") }}>MG</button>
-        <button id="PA" onClick={() => { filtrar("PA") }}>PA</button>
-        <button id="PB" onClick={() => { filtrar("PB") }}>PB</button>
-        <button id="PR" onClick={() => { filtrar("PR") }}>PR</button>
-        <button id="PE" onClick={() => { filtrar("PE") }}>PE</button>
-        <button id="PI" onClick={() => { filtrar("PI") }}>PI</button>
-        <button id="RJ" onClick={() => { filtrar("RJ") }}>RJ</button>
-        <button id="RN" onClick={() => { filtrar("RN") }}>RN</button>
-        <button id="RS" onClick={() => { filtrar("RS") }}>RS</button>
-        <button id="RO" onClick={() => { filtrar("RO") }}>RO</button>
-        <button id="RR" onClick={() => { filtrar("RR") }}>RR</button>
-        <button id="SC" onClick={() => { filtrar("SC") }}>SC</button>
-        <button id="SP" onClick={() => { filtrar("SP") }}>SP</button>
-        <button id="SE" onClick={() => { filtrar("SE") }}>SE</button>
-        <button id="TO" onClick={() => { filtrar("TO") }}>TO</button>
-        <button id="AC" onClick={() => { limpar() }}>Limpar</button>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label>UF</label>
+          <select {...register("UF")}>
+            <option selected>Todos</option>
+            <option value="AC">AC</option>
+            <option value="AL">AL</option>
+            <option value="AP">AP</option>
+            <option value="AM">AM</option>
+            <option value="BA">BA</option>
+            <option value="CE">CE</option>
+            <option value="DF">DF</option>
+            <option value="ES">ES</option>
+            <option value="GO">GO</option>
+            <option value="MA">MA</option>
+            <option value="MT">MT</option>
+            <option value="MS">MS</option>
+            <option value="MG">MG</option>
+            <option value="PA">PA</option>
+            <option value="PB">PB</option>
+            <option value="PR">PR</option>
+            <option value="PE">PE</option>
+            <option value="PI">PI</option>
+            <option value="RJ">RJ</option>
+            <option value="RN">RN</option>
+            <option value="RS">RS</option>
+            <option value="RO">RO</option>
+            <option value="RR">RR</option>
+            <option value="SC">SC</option>
+            <option value="SP">SP</option>
+            <option value="SE">SE</option>
+            <option value="TO">TO</option>
+            <option value="AC">Limpar</option>
+          </select>
+          <label>Tipo</label>
+          <select  {...register("TIPO")}>
+            <option value="UTI">Todos</option>
+            <option value="UTI">UTI</option>
+            <option value="TRANSPORTE">TRANSPORTE</option>
+            <option value="TRANSPORTE USA">TRANSPORTE USA</option>
+          </select>
+          <button type="submit">Filtrar valores</button>
+        </form>
+
       </div>
       <div id="bloco-graficos">
         <div id="chart2"></div>
